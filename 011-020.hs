5d744379c3bf13d4a5740a0b172759426084c605
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2024-04-12 19:59:05 +0200

    Add files via upload

--- 011-020.hs
+++ 011-020.hs
@@ -58,7 +58,37 @@ dropEvery [] _ = []
 dropEvery xs n = (take (n - 1) xs) ++ (dropEvery (drop n xs) n)
 
 
--- Question 17
+-- Question 17: Without any predefined predicates.
+split :: [a] -> Int -> ([a], [a])
+split xs n = go [] xs 0
+  where
+    go left right@(r:rs) i
+        | i == n    = (left, right)
+        | otherwise = go (left ++ [r]) rs (i + 1) 
+
+
+-- Question 18
+slice :: [a] -> Int -> Int -> [a]
+slice xs i j
+    | i >= 1 && j <= length xs && i <= j = drop (i - 1) $ take j xs
+    | otherwise                          = error "slice: OOB."
+
+
+-- Question 19
+rotate :: [a] -> Int -> [a]
+rotate xs n = (drop k xs) ++ (take k xs)
+  where
+    k = n `mod` length xs
+
+
+-- Question 20
+removeAt :: Int -> [a] -> (a, [a])
+removeAt n xs
+    | n >= 1 && n <= length xs = (r, xs')
+    | otherwise                = error "removeAt: OOB."
+  where
+    r   = xs !! (n - 1)
+    xs' = take (n - 1) xs ++ drop n xs
 
 main = do
     print $ "Question 11: " ++ show (encodeModified "aaaabccaadeeee" == [Multiple 4 'a',Single 'b',Multiple 2 'c', Multiple 2 'a',Single 'd',Multiple 4 'e'])
@@ -67,6 +97,12 @@ main = do
     print $ "Question 14: " ++ show (dupli [1, 2, 3] == [1, 1, 2, 2, 3, 3])
     print $ "Question 15: " ++ show (repli "abc" 3 == "aaabbbccc")
     print $ "Question 16: " ++ show (dropEvery "abcdefghik" 3 == "abdeghk")
+    print $ "Question 17: " ++ show (split "abcdefghik" 3 == ("abc", "defghik"))
+    print $ "Question 18: " ++ show (slice ['a'..'k'] 3 7 == ['c'..'g'])
+    print $ "Question 19: " ++ show (rotate ['a'..'h'] 3 == "defghabc")
+    print $ "Question 19: " ++ show (rotate ['a'..'h'] (-2) == "ghabcdef")
+    print $ "Question 20: " ++ show (removeAt 2 "abcd" == ('b',"acd")q)
+    
     
     print $ "---"
 
